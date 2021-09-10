e867ef4e145ad6942844509487676b1bd0e87d2a
HassanSaeed7 <Hsaeed1007@gmail.com>
2021-09-10 02:14:33 -0700

    g

---
+++ file.js
@@ -0,0 +1,23 @@
+// let hands = ["rock", "paper", "scissors"] 
+
+// function randomSign() {
+//     let randomIndex = Math.floor(Math.random() * 3)
+//     return hands[randomIndex]
+// }
+// console.log(randomSign())
+
+// let fruit = ["apple", "orange", "apple", "orange", "orange"]
+// let appleShelf = document.getElementById("apple-shelf")
+// let orangeShelf = document.getElementById("orange-shelf")
+
+// function sorter() {
+//     for (let i = 0; i < fruit.length; i++) {
+//         if (fruit[i] === "apple") {
+//             appleShelf.textContent += fruit[i] + " "
+//         } else {
+//             orangeShelf.textContent += fruit[i] + " "
+//         }
+//     }
+// }
+// sorter();
+
